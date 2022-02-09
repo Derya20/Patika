fe54071b2533ed5865975f9b33a2802e8d6de672
Derya20 <yldr_1599@hotmail.com>
2022-02-09 04:17:44 +0300

    PatikaPractice

---
+++ src/BodyMassIndex.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    /*
+    Kilo (kg) / Boy(m) * Boy(m)
+     */
+    public static void main(String[] args) {
+
+        double kilo,boy, index;
+
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        boy=scan.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz :");
+        kilo=scan.nextDouble();
+        index =kilo/(boy*boy);
+        System.out.print("Vücut Kitle İndeksiniz : "+ index);
+
+    }
+}
